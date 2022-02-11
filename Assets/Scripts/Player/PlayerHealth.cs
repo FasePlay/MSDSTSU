851aa0c8863bf3158c0cae047e6f98f73effd873
reallyrealkey <kbratishko@gmail.com>
2022-02-11 22:56:51 +0200

    KEY PICKUPS! wip - door interactions
    u can pick up stuff now (only keys rn)

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,14 +3,19 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    //cooldowns
     public float shieldTimer = 5f;
     public float invincibilityTimer = 2f;
+    
+    //ui stats
     public GameObject shieldObj;
     public GameObject heart;
 
+    //animators for hp ui
     public Animator shieldAnimator;
     public Animator playerAnimator;
 
+    
     public GameOver gameOver;
 
     bool Shield = true;
@@ -33,19 +38,23 @@ public class PlayerHealth : MonoBehaviour
             shieldAnimator.SetBool("damaged",true);
             playerAnimator.SetTrigger("damaged");
             Invoke("ShieldInvoke", shieldTimer);
-            Invoke("invincible = false;", invincibilityTimer);
+            Invoke("InvincibilityEnd", invincibilityTimer);
         }
         else
         {
             playerAnimator.SetBool("dead", true);
             gameOver.EndGame();
         }
-
     }
-    private void ShieldInvoke()
+    void ShieldInvoke()
     {
         Shield = true;
         shieldAnimator.SetBool("damaged", false);
     }
+    private void InvincibilityEnd()
+    {
+        invincible = false;
+        // anims here please sanya  - /shrug
+     }
 }
 
--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -5,16 +5,35 @@ public class PlayerInventory : MonoBehaviour
     //inventory items
     [SerializeField] private int goldKeys = 0;
     [SerializeField] private int silverKeys = 0;
-    
+    public Animator keyAnimator;
+    public GameObject goldKey;
+    public int playerInput = 1;
+
     //player stats
     public int playerLevel = 1;
 
-    public void OnCollisionEnter2D(Collision2D collisioninfo)
+    public void OnTriggerEnter2D(Collider2D col)
     {
-        if (!collisioninfo.collider.CompareTag("goldKey"))
+        switch (col.tag)
         {
-
+            case "goldKey":
+                {
+                    goldKeys++;
+                    keyAnimator.SetBool("isPickedUp", true);
+                    goldKey.GetComponent<BoxCollider2D>().enabled = false;
+                    break;
+                }
+            case "silverKey":
+                {
+                    silverKeys++;
+                    break;
+                }
+            default:
+                {
+                    print("tf did u collide with?");
+                    break;
+                }
         }
+        print($"u got {goldKeys} gold keys");
     }
-
 }
